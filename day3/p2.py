64f45b50eefad2f12a80986a79e18b67fbcf8648
saipratap414 <saipratapchalla123@gmail.com>
2025-10-08 11:09:49 +0530

    list

--- day3/p2.py
+++ day3/p2.py
@@ -1,9 +1,8 @@
 list1 = [2,3,4,5,6]
 list2 = [9,8,1,7,3]
 
-list3 = list1.copy()  
-list3.extend(list2)
-print("Merged list:", list3)
+list1.extend(list2)
+print("Merged list:", list1)
 
-list3.sort()
-print("Sorted merged list:", list3)
+list1.sort()
+print("Sorted merged list:", list1)
