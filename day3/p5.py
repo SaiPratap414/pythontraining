bce4d6daa9c3c9b3504b7cf0fccab958c9d5139c
saipratap414 <saipratapchalla123@gmail.com>
2025-10-08 11:55:13 +0530

    tup

---
+++ day3/p5.py
@@ -0,0 +1,7 @@
+employees =("sai", "suman", "chandu", "sai")
+print(employees[0])
+print(employees.index("sai"))
+print(employees.count("sai"))
+
+for i in employees:
+    print(i)
---
+++ day3/p6.py
@@ -0,0 +1,14 @@
+numbers = (3,2,4,5)
+print(len(numbers))
+print(max(numbers))
+print(min(numbers))
+print(sorted(numbers))
+print(sum(numbers))
+print(sum(numbers)/len(numbers))
+
+names =["sai", "pratap"]
+new = tuple(names)
+print(new)
+
+list1 = list(numbers)
+print(list1)
