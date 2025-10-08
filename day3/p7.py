defb3d1a4fc1e3344dd2fb9585dfc14e1eb7b2ad
saipratap414 <saipratapchalla123@gmail.com>
2025-10-08 12:06:16 +0530

    set

---
+++ day3/p7.py
@@ -0,0 +1,12 @@
+names= {"sai", "pratap", "pooja", "shreyanka", "sai"}
+print(names)
+names.add("gautham")
+names.remove("sai")
+for i in names:
+    print(i)
+num=[2,3,2,3,5,6]
+new = set(num)
+new =frozenset(num)
+print(new)
+new.add(9)
+print(new)
