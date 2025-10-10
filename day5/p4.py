306179b2478512bc783f70bafa6b851fda2a7cf7
saipratap414 <saipratapchalla123@gmail.com>
2025-10-10 11:45:52 +0530

    reduce

--- day5/p4.py
+++ day5/p4.py
@@ -1,4 +1,19 @@
-def calc(x):
-    return x+2
-result = map(calc,[20,23,28])
-print(list(result))
+# def calc(x):
+#     return x+2
+# result = map(calc,[20,23,28])
+# print(list(result))
+
+# def calc(x):
+#     return x>20
+# list1= [20,23,28]
+# list2 = filter(calc,list1)
+# print(list(list2))
+
+
+
+from functools import reduce
+def calc(x,y):
+    return x+y
+list1= [20,23,28]
+list2 = reduce(calc,list1)
+print(list2)
